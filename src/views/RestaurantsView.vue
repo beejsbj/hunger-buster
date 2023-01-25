87c7d08601651d2739e295adf69f4b7b09cd1f67
beejsbj <burooj.bj@gmail.com>
2023-01-25 18:43:56 -0500

    Render Restaurants from data

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -4,40 +4,44 @@ const shop = useShopStore();
 </script>
 
 <template>
-  <h2 class="attention-voice">Items</h2>
-  <ul>
-    <li v-for="item in shop.list.items">
-      <item-card>
-        <h3 class="solid-voice">
-          {{ item.name }}
-        </h3>
-        <picture>
-          <img :src="item.image" alt="" />
-        </picture>
-        <div>
-          <p>${{ item.price }}</p>
-          <button @click="shop.add(item)">Add to Cart</button>
-          <a :href="'item/' + item.slug">MORE</a>
-        </div>
-      </item-card>
-    </li>
-  </ul>
+	<h2 class="attention-voice">Restaurants</h2>
+	<ul>
+		<li v-for="restaurant in shop.list.restaurants">
+			<restaurant-card>
+				<h3 class="solid-voice">
+					{{ restaurant.name }}
+				</h3>
+				<picture>
+					<img
+						:src="
+							restaurant.image ?? 'https://peprojects.dev/images/square.jpg'
+						"
+						alt="https://peprojects.dev/images/square.jpg"
+					/>
+				</picture>
+				<div>
+					<p>{{ "$".repeat(restaurant.priceLevel) }}</p>
+					<a :href="'restaurant/' + restaurant.slug">MORE</a>
+				</div>
+			</restaurant-card>
+		</li>
+	</ul>
 </template>
 
 <style scoped>
-item-card {
-  display: grid;
-  gap: 5px;
+restaurant-card {
+	display: grid;
+	gap: 5px;
 }
-item-card div {
-  display: flex;
-  justify-content: space-between;
+restaurant-card div {
+	display: flex;
+	justify-content: space-between;
 }
 ul {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 
-  gap: 10px;
+	gap: 10px;
 }
 </style>
