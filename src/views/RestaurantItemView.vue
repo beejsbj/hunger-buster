6a7fed86a6c7409750a7836d39509bdb63ef0846
beejsbj <burooj.bj@gmail.com>
2023-01-25 22:38:05 -0500

    Render Options

--- src/views/RestaurantItemView.vue
+++ src/views/RestaurantItemView.vue
@@ -19,3 +19,53 @@
 	});
 	const item = reactive(foundItem);
 </script>
+		<required-options v-if="item.requiredOptions">
+			<h2 class="attention-voice">Required Options</h2>
+			<ul
+				class="radio-list"
+				v-for="(options, key) in item.requiredOptions"
+			>
+				<h3 class="solid-voice">
+					Please Select {{ key }}
+					<span>required</span>
+					{{ selected }}
+				</h3>
+				<li
+					class="radio-item"
+					v-for="option in options"
+				>
+					<label :for="option.name">
+						{{ option.name ?? option }}
+					</label>
+					<input
+						required
+						:v-model="selected"
+						:name="key"
+						type="radio"
+						:value="option.name"
+					/>
+				</li>
+			</ul>
+		</required-options>
+
+		<extra-options v-if="item.extraOptions">
+			<h2 class="attention-voice">Extra Options</h2>
+			<ul
+				class="radio-list"
+				v-for="(options, key) in item.extraOptions"
+			>
+				<h3 class="solid-voice">Please Select {{ key }}</h3>
+				<li
+					class="radio-item"
+					v-for="option in options"
+				>
+					<label :for="option.name">{{ option.name }}</label>
+					<input
+						:v-model="selected"
+						:name="key"
+						type="checkbox"
+						:value="option.name"
+					/>
+				</li>
+			</ul>
+		</extra-options>
