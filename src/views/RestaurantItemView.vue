6d5ddc1fa073d8d5d3cc6e88e77ceb3551954a7c
beejsbj <burooj.bj@gmail.com>
2023-01-25 22:37:31 -0500

    Find restaurant and item from slugs

---
+++ src/views/RestaurantItemView.vue
@@ -0,0 +1,21 @@
+<script setup>
+	import { reactive } from "vue";
+	import { useRoute } from "vue-router";
+	import { useShopStore } from "../stores/shop.js";
+
+	const route = useRoute();
+	const shop = useShopStore();
+
+	const restaurant = shop.list.restaurants.find(function (restaurant) {
+		return (
+			route.params.slug == restaurant.id ||
+			route.params.slug == restaurant.slug
+		);
+	});
+	const foundItem = restaurant.menuItems.find(function (item) {
+		return (
+			route.params.itemSlug == item.id || route.params.itemSlug == item.slug
+		);
+	});
+	const item = reactive(foundItem);
+</script>
