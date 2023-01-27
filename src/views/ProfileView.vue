eef1daa9a299710ae80200899066fdf7ac861ee6
beejsbj <burooj.bj@gmail.com>
2023-01-27 18:50:38 -0500

    Init profile view and begin subroute work

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,42 @@
+<script setup>
+	import { useUserStore } from "../stores/user.js";
+	const user = useUserStore();
+</script>
+
+<template>
+	<profile-view>
+		<header class="profile">
+			<nav>
+				<RouterLink to="/user/about">About</RouterLink>
+				<RouterLink to="/user/favorites">Profile</RouterLink>
+			</nav>
+		</header>
+		<h1 class="loud-voice">
+			{{ user.profile.name }}
+		</h1>
+		<picture>
+			<img
+				:src="user.profile.image"
+				alt=""
+			/>
+		</picture>
+
+		<favorites-module>
+			<h2 class="attention-voice">Fav Rests</h2>
+			<ul class="favorites">
+				<li v-for="restaurant in user.profile.favoriteRestaurants">
+					{{ restaurant.name }}
+				</li>
+			</ul>
+		</favorites-module>
+		<favorites-module>
+			<h2 class="attention-voice">Fav Items</h2>
+			<ul class="favorites">
+				<li v-for="item in user.profile.favoriteItems">
+					{{ item.name }}
+				</li>
+			</ul>
+		</favorites-module>
+	</profile-view>
+</template>
+<style></style>
