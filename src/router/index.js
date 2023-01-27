a37f4bb4e0b2966fcbf717a909124e99ae326509
beejsbj <burooj.bj@gmail.com>
2023-01-27 16:16:21 -0500

    Add router for user

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const router = createRouter({
 			component: () => import("../views/AboutView.vue"),
 		},
 		{
+			path: "/user",
+			name: "user",
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import("../views/ProfileView.vue"),
+		},
+		{
 			path: "/restaurants",
 			name: "Restaurants",
 			component: () => import("../views/RestaurantsView.vue"),
