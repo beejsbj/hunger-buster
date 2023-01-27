8fb6c2c4d2cf6fdc3c56042b6e3606d09edec82a
beejsbj <burooj.bj@gmail.com>
2023-01-27 16:15:03 -0500

    Create users shop and favorite restaurant function

---
+++ src/stores/user.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { reactive } from "vue";
+
+export const useUserStore = defineStore("user", () => {
+	const profile = reactive({
+		name: "Burooj",
+		image: "https://peprojects.dev/images/square.jpg",
+		favoriteRestaurants: [],
+		favoriteItems: [],
+	});
+
+	function favoriteRestaurant(restaurant) {
+		if (profile.favoriteRestaurants.includes(restaurant)) {
+			let index = profile.favoriteRestaurants.indexOf(restaurant);
+			profile.favoriteRestaurants.splice(index, 1);
+		} else {
+			profile.favoriteRestaurants.push(restaurant);
+		}
+	}
