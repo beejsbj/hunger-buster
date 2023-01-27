abb5ff01a810b5387e7c7d182bd1ccbfbac0d92b
beejsbj <burooj.bj@gmail.com>
2023-01-27 02:17:08 -0500

    Import data from locaal json

--- src/stores/shop.js
+++ src/stores/shop.js
@@ -1,98 +1,76 @@
 import { ref, computed, reactive, watch, onMounted } from "vue";
 import { defineStore } from "pinia";
-import itemsData from "../assets/static-data/items.json";
+import restaurantsData from "../assets/static-data/restaurants.json";
 import { v4 as uuid } from "uuid";
 import slug from "slug";
 
 if (!localStorage.restaurants) {
-	const data = fetch(
-		"https://raw.githubusercontent.com/perpetual-education/restaurants-data/main/data.json"
-	);
-	data
-		.then(function (data) {
-			return data.json();
-		})
-		.then(function (json) {
-			localStorage.restaurants = JSON.stringify(json);
-		});
+	localStorage.restaurants = JSON.stringify(restaurantsData);
+	// const data = fetch(
+	// 	"https://raw.githubusercontent.com/perpetual-education/restaurants-data/main/data.json"
+	// );
+	// data
+	// 	.then(function (data) {
+	// 		return data.json();
+	// 	})
+	// 	.then(function (json) {
+	// 		localStorage.restaurants = JSON.stringify(json);
+	// 	});
 }
 
 export const useShopStore = defineStore("shop", () => {
-	const list = reactive({
-		restaurants: loadRestaurants(),
-		cart: loadCart() ? reactive(loadCart()) : reactive([]),
-	});
+	const restaurants = reactive(loadRestaurants());
 
-	const total = computed(function () {
-		return list.cart
-			.reduce(function (total, restaurant) {
-				return total + restaurant.price * restaurant.quantity;
-			}, 0)
-			.toFixed(2);
-	});
+	// const total = computed(function () {
+	// 	return list.cart
+	// 		.reduce(function (total, restaurant) {
+	// 			return total + restaurant.price * restaurant.quantity;
+	// 		}, 0)
+	// 		.toFixed(2);
+	// });
 
-	function create(restaurant) {
-		let record = {
-			name: restaurant.name,
-			price: restaurant.price,
-			image: restaurant.image,
-			id: uuid(),
-			slug: slug(restaurant.name),
-		};
-		list.restaurants.push(record);
-		restaurant.name = "";
-		restaurant.price = 0;
-		restaurant.image = "";
-	}
-
-	function erase(restaurant) {
-		list.restaurants = list.restaurants.filter(function (storeItem) {
-			return storeItem.id != restaurant.id;
+	function findInCart(item) {
+		return restaurant.id.find(function (cartItem) {
+			return cartItem.id == item.id;
 		});
 	}
 
-	function findInCart(restaurant) {
-		return list.cart.find(function (cartItem) {
-			return cartItem.id == restaurant.id;
-		});
+	function findRestaurant(id) {
+		return restaurants.findIndex((restaurant) => restaurant.id == id);
 	}
 
-	function add(restaurant) {
-		let found = findInCart(restaurant);
-		if (found) {
-			quantityIncrement(found);
-		} else {
-			//push restaurant object into cart
-			let record = restaurant;
-			list.cart.push({
-				id: restaurant.id,
-				name: restaurant.name,
-				price: restaurant.price,
-				quantity: 1,
-				image: restaurant.image,
-			});
+	function add(item, restaurant) {
+		let id = findRestaurant(restaurant.id);
+
+		if (!restaurants[id].cart) {
+			restaurants[id].cart = [];
 		}
+
+		item.quantity = 1;
+		restaurants[id].cart.push(item);
 	}
 
-	function remove(restaurant) {
-		list.cart = list.cart.filter(function (cartItem) {
-			return cartItem.id != restaurant.id;
+	function remove(item, restaurant) {
+		let id = findRestaurant(restaurant.id);
+
+		restaurants[id].cart = restaurants[id].cart.filter(function (cartItem) {
+			return cartItem.id != item.id;
 		});
 	}
 
-	function quantityIncrement(restaurant) {
-		restaurant.quantity++;
+	function quantityIncrement(item) {
+		item.quantity++;
 	}
 
-	function quantityDecrement(restaurant) {
-		restaurant.quantity--;
-		if (restaurant.quantity < 1) {
-			remove(restaurant);
+	function quantityDecrement(item) {
+		item.quantity--;
+		if (item.quantity < 1) {
+			remove(item);
 		}
 	}
 
 	function saveRestaurants() {
-		localStorage.setItem("restaurants", JSON.stringify(list.restaurants));
+		localStorage.setItem("restaurants", JSON.stringify(restaurants));
 		console.log("Restaurants Saved");
 	}
 
@@ -101,18 +79,7 @@ export const useShopStore = defineStore("shop", () => {
 		return JSON.parse(restaurantsStr);
 	}
 
-	function saveCart() {
-		localStorage.setItem("cart", JSON.stringify(list.cart));
-		console.log("Cart saved");
-	}
-
-	function loadCart() {
-		var cartStr = localStorage.getItem("cart");
-		return JSON.parse(cartStr);
-	}
-
-	watch(list, function () {
-		saveCart();
+	watch(restaurants, function () {
 		saveRestaurants();
 	});
 
@@ -121,10 +88,8 @@ export const useShopStore = defineStore("shop", () => {
 	});
 
 	return {
-		list,
-		total,
-		create,
-		erase,
+		restaurants,
+		// total,
 		add,
 		remove,
 		quantityIncrement,
