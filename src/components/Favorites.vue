faefc20e60413c6a7e3832ba50398ff16afb4e3a
beejsbj <burooj.bj@gmail.com>
2023-01-28 17:39:04 -0500

    Refactor into favorites component

---
+++ src/components/Favorites.vue
@@ -0,0 +1,12 @@
+<script setup>
+	import { useUserStore } from "../stores/user.js";
+	const user = useUserStore();
+</script>
+<favorites-module>
+			<h2 class="attention-voice">Fav Rests</h2>
+			<ul class="favorites">
+				<li v-for="restaurant in user.profile.favoriteRestaurants">
+					{{ restaurant.name }}
+				</li>
+			</ul>
+		</favorites-module>
