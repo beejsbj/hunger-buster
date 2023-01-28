584471abaacea03e1d1d9f4b67d83a019cca18ac
beejsbj <burooj.bj@gmail.com>
2023-01-28 18:56:49 -0500

    Added template eelement wrapper

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -2,11 +2,21 @@
 	import { useUserStore } from "../stores/user.js";
 	const user = useUserStore();
 </script>
-<favorites-module>
-			<h2 class="attention-voice">Fav Rests</h2>
-			<ul class="favorites">
-				<li v-for="restaurant in user.profile.favoriteRestaurants">
-					{{ restaurant.name }}
-				</li>
-			</ul>
-		</favorites-module>
+<template>
+	<favorites-module>
+		<h2 class="attention-voice">Fav Rests</h2>
+		<ul class="favorites">
+			<li v-for="restaurant in user.profile.favoriteRestaurants">
+				{{ restaurant.name }}
+			</li>
+		</ul>
+	</favorites-module>
+	<favorites-module>
+		<h2 class="attention-voice">Fav Items</h2>
+		<ul class="favorites">
+			<li v-for="item in user.profile.favoriteItems">
+				{{ item.name }}
+			</li>
+		</ul>
+	</favorites-module>
+</template>
