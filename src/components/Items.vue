97c1b425895bc0dcd05648f2fdaa51b712f7d8a3
beejsbj <burooj.bj@gmail.com>
2023-01-28 18:56:32 -0500

    Refactored items to a compoennt

---
+++ src/components/Items.vue
@@ -0,0 +1,61 @@
+<script setup>
+	import { useRoute, useRouter } from "vue-router";
+
+	import { useShopStore } from "../stores/shop.js";
+
+	const route = useRoute();
+	const router = useRouter();
+	const shop = useShopStore();
+
+	const restaurant = shop.restaurants.find(function (restaurant) {
+		return (
+			route.params.restaurantSlug == restaurant.id ||
+			route.params.restaurantSlug == restaurant.slug
+		);
+	});
+</script>
+<template>
+	<restaurant-card>
+		<menu-items>
+			<ul>
+				<li v-for="item in restaurant.menuItems">
+					<item-card>
+						<h4>{{ item.name }}</h4>
+						<picture>
+							<img
+								:src="
+									item.image ??
+									'https://peprojects.dev/images/square.jpg'
+								"
+								alt="https://peprojects.dev/images/square.jpg"
+							/>
+						</picture>
+
+						<p>${{ item.price }}</p>
+						<RouterLink :to="`${restaurant.slug}/${item.slug}`"
+							>MORE</RouterLink
+						>
+					</item-card>
+				</li>
+			</ul>
+		</menu-items>
+	</restaurant-card>
+</template>
+<style scoped>
+	restaurant-card {
+		display: grid;
+		gap: 5px;
+	}
+	restaurant-card div {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	item-card {
+		display: grid;
+		grid-template-columns: 1fr 0.3fr;
+	}
+	a {
+		color: salmon;
+	}
+</style>
