821c0b59b562e134163e438ca7639d06e5e143bb
beejsbj <burooj.bj@gmail.com>
2023-01-30 12:43:00 -0500

    Create not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,17 @@
+<script>
+	var header = [
+		navigator.platform,
+		navigator.userAgent,
+		navigator.appVersion,
+		navigator.vendor,
+		window.opera,
+	];
+	const platform = console.log(typeof navigator.platform);
+	// var agent = header.join(" ");
+	// var os = this.matchItem(agent, os);
+	// var browser = this.matchItem(agent, browser);
+</script>
 <template>
 	<h1>NOT FOUND</h1>
+	<p>YOu on your {{ platform }}</p>
 </template>
