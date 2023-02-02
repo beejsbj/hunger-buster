8961406fff035fae9001cd01b33f1d0e4c32aa9f
beejsbj <burooj.bj@gmail.com>
2023-02-02 00:48:57 -0500

    Add stryling for restaurants list

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -17,66 +17,75 @@
 </script>
 
 <template>
-	<h2 class="attention-voice">Restaurants</h2>
-	<input-field>
-		<input
-			type="text"
-			v-model="filter"
-			placeholder="Filter items"
-		/>
-	</input-field>
+	<article>
+		<h2 class="attention-voice">Restaurants</h2>
+		<input-field>
+			<input
+				class="filter"
+				type="text"
+				v-model="filter"
+				placeholder="Filter Restaurants"
+			/>
+		</input-field>
 
-	<ul>
-		<li v-for="restaurant in filtered">
-			<restaurant-card>
-				<h3 class="solid-voice">
-					{{ restaurant.name }}
-				</h3>
-				<picture>
-					<img
-						:src="
-							restaurant.image ??
-							'https://peprojects.dev/images/square.jpg'
-						"
-						alt="https://peprojects.dev/images/square.jpg"
-					/>
-				</picture>
-				<div>
-					<p>{{ "$".repeat(restaurant.priceLevel) }}</p>
-					<a :href="'/restaurant/' + restaurant.slug">MORE</a>
-					<button
-						:class="{
-							favorite:
-								user.profile.favoriteRestaurants.includes(restaurant),
-						}"
-						@click="user.favoriteRestaurant(restaurant)"
-					>
-						Heart
-					</button>
-				</div>
-			</restaurant-card>
-		</li>
-	</ul>
+		<ul class="restaurant-list">
+			<li v-for="restaurant in filtered">
+				<restaurant-card>
+					<h3 class="solid-voice">
+						{{ restaurant.name }}
+					</h3>
+					<picture>
+						<img
+							:src="
+								restaurant.image ??
+								'https://peprojects.dev/images/square.jpg'
+							"
+							alt="https://peprojects.dev/images/square.jpg"
+						/>
+					</picture>
+					<div>
+						<p>{{ "$".repeat(restaurant.priceLevel) }}</p>
+						<a :href="'/restaurant/' + restaurant.slug">MORE</a>
+						<button
+							:class="{
+								favorite:
+									user.profile.favoriteRestaurants.includes(
+										restaurant
+									),
+							}"
+							@click="user.favoriteRestaurant(restaurant)"
+						>
+							Heart
+						</button>
+					</div>
+				</restaurant-card>
+			</li>
+		</ul>
+	</article>
 </template>
 
 <style scoped>
 	restaurant-card {
 		display: grid;
-		gap: 5px;
-	}
-	restaurant-card div {
-		display: flex;
-		justify-content: space-between;
-	}
-	ul {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-
 		gap: 10px;
 	}
+	restaurant-card div {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		align-items: center;
+		justify-content: space-between;
+		padding: 10px;
+	}
+	ul.restaurant-list {
+		display: grid;
 
-	button.favorite {
-		background: yellow;
+		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+
+		gap: 20px;
+	}
+
+	article {
+		display: grid;
+		gap: 20px;
 	}
 </style>
