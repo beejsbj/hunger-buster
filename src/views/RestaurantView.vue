c0638fe65284b3bc68c69c8e56cdf376364b9a5b
beejsbj <burooj.bj@gmail.com>
2023-02-02 00:48:52 -0500

    Add stryling for restaurant

--- src/views/RestaurantView.vue
+++ src/views/RestaurantView.vue
@@ -15,43 +15,62 @@
 </script>
 
 <template>
-	<header class="restaurant">
-		<nav class="restaurant-menu">
-			<RouterLink :to="'/restaurant/' + restaurant.slug">Items</RouterLink>
-			<RouterLink
-				:to="'/restaurant/' + restaurant.slug + '/cart'"
-				class="cart"
-			>
-				Cart
-				<span :class="{ cartCount: restaurant.cart.length }">
-					{{ restaurant.cart.length }}
-				</span>
-			</RouterLink>
-		</nav>
-		<restaurant-banner>
-			<h2 class="loud-voice">
-				{{ restaurant.name }}
-			</h2>
-			<picture>
-				<img
-					:src="
-						restaurant.image ?? 'https://peprojects.dev/images/square.jpg'
-					"
-					alt="https://peprojects.dev/images/square.jpg"
-				/>
-			</picture>
-		</restaurant-banner>
-		<h3 class="attention-voice">{{ "$".repeat(restaurant.priceLevel) }}</h3>
-	</header>
-	<sub-view>
-		<RouterView />
-	</sub-view>
+	<article>
+		<header class="restaurant">
+			<nav class="restaurant-menu">
+				<RouterLink :to="'/restaurant/' + restaurant.slug"
+					>Items</RouterLink
+				>
+				<RouterLink
+					:to="'/restaurant/' + restaurant.slug + '/cart'"
+					class="cart"
+				>
+					Cart
+					<span :class="{ cartCount: restaurant.cart.length }">
+						<span>
+							{{ restaurant.cart.length }}
+						</span>
+					</span>
+				</RouterLink>
+			</nav>
+			<restaurant-banner>
+				<text-content>
+					<h2 class="loud-voice">
+						{{ restaurant.name }}
+					</h2>
+					<h3 class="firm-voice">
+						{{ "$".repeat(restaurant.priceLevel) }}
+					</h3>
+				</text-content>
+				<picture>
+					<img
+						:src="
+							restaurant.image ??
+							'https://peprojects.dev/images/square.jpg'
+						"
+						alt="https://peprojects.dev/images/square.jpg"
+					/>
+				</picture>
+			</restaurant-banner>
+		</header>
+		<sub-view>
+			<RouterView />
+		</sub-view>
+	</article>
 </template>
 
 <style lang="scss" scoped>
+	header,
+	article {
+		display: grid;
+		gap: 20px;
+	}
 	restaurant-banner {
 		display: grid;
-		grid-template-columns: 1fr 0.3fr;
+		grid-template-columns: 1fr 0.2fr;
+		picture {
+			grid-row: span 2;
+		}
 	}
 	a {
 		&.cart {
@@ -63,13 +82,23 @@
 
 			.cartCount {
 				display: initial;
-				font-size: 0.8em;
 				/*	vertical-align: super;*/
 				background: yellow;
 				color: black;
 				position: absolute;
 				top: -10px;
 				right: -5px;
+				padding: 10px;
+				border-radius: 50%;
+
+				span {
+					font-size: 0.9em;
+					display: initial;
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%, -50%);
+				}
 			}
 		}
 	}
