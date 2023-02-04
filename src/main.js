f02555df508c825e5efcab50580ca29fd5249413
beejsbj <burooj.bj@gmail.com>
2023-02-03 20:18:20 -0500

    Importing Firebase app into main and using it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { firebaseApp } from "./firebase.js";
+import { VueFire, VueFireAuth } from "vuefire";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,4 +13,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
+app.use(VueFire, {
+	firebaseApp,
+	modules: [VueFireAuth()],
+});
 app.mount("#app");
