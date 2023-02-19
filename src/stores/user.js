4cb311f55e0fe16f7d9081299f6cfa547d21b0ae
beejsbj <burooj.bj@gmail.com>
2023-02-19 14:49:11 -0500

    get user data from auth

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,29 +1,47 @@
-import { defineStore } from "pinia";
-import { reactive, watch } from "vue";
-import { useCurrentUser } from "vuefire";
+//imports
+import { useCurrentUser, useDocument } from "vuefire";
 import {
 	getAuth,
 	createUserWithEmailAndPassword,
 	signOut,
 	signInWithEmailAndPassword,
+	updateProfile,
 } from "firebase/auth";
+import { collection } from "firebase/firestore";
 
-import { useRouter } from "vue-router";
-
+//setup
 const router = useRouter();
+const db = useFirestore();
 
-export const useUserStore = defineStore("user", () => {
-	const auth = getAuth();
-
+function getUser() {
 	const current = useCurrentUser();
 
-	const profile = reactive({
-		name: "Burooj",
-		image: "https://peprojects.dev/images/square.jpg",
-		favoriteRestaurants: [],
-		favoriteItems: [],
+	const userRef = computed(function () {
+		if (current.value?.uid) {
+			return doc(collection(db, "users"), `user_${current.value.uid}`);
+		}
 	});
+	const userDoc = useDocument(userRef);
 
+	//compute returns
+	const roles = computed(() => userDoc.value?.roles);
+	const id = computed(() => userDoc.value?.id);
+	const profile = computed(() => userDoc.value?.profile);
+	return { id, roles, profile };
+}
+
+
+// /store
+export const useUserStore = defineStore("user", () => {
+	const auth = getAuth();
+	const current = useCurrentUser();
+	const { id, roles, profile } = getUser();
+
+	//
+
+	//
+
+	// functions
 	function favoriteRestaurant(restaurant) {
 		if (profile.favoriteRestaurants.includes(restaurant)) {
 			let index = profile.favoriteRestaurants.indexOf(restaurant);
@@ -32,6 +50,7 @@ export const useUserStore = defineStore("user", () => {
 			profile.favoriteRestaurants.push(restaurant);
 		}
 	}
+
 	function favoriteItem(item) {
 		if (profile.favoriteItems.includes(item)) {
 			let index = profile.favoriteItems.indexOf(item);
@@ -45,7 +64,17 @@ export const useUserStore = defineStore("user", () => {
 		createUserWithEmailAndPassword(auth, form.email, form.password)
 			.then((userCredential) => {
 				const user = userCredential.user;
-				console.log(user);
+				setDoc(doc(db, "users", `user_${user.uid}`), {
+					id: user.uid,
+					roles: {},
+					profile: {
+						displayName: form.displayName ?? "Display Name",
+						image: "https://peprojects.dev/images/square.jpg",
+						description: "lorem",
+						favoriteRestaurants: [],
+						favoriteItems: [],
+					},
+				});
 			})
 			.catch((error) => {
 				console.log(error.code, error.message);
@@ -57,7 +86,6 @@ export const useUserStore = defineStore("user", () => {
 		signInWithEmailAndPassword(auth, form.email, form.password)
 			.then((userCredential) => {
 				const user = userCredential.user;
-				console.log(user);
 			})
 			.catch((error) => {
 				console.log(error.code, error.message);
@@ -70,23 +98,48 @@ export const useUserStore = defineStore("user", () => {
 			.then(() => {
 				router.push({ name: "home" });
 			})
-			.catch((error) => {
-				// An error happened.
-			});
+			.catch((error) => {});
 	}
 
 	function clearForm(form) {
 		form.email = "";
 		form.password = "";
 	}
+	
+
+	// function saveUser() {
+	// 	updateProfile(auth.currentUser, {
+	// 		displayName: auth.currentUser.displayName ?? "Display Name",
+	// 		photoURL:
+	// 			auth.currentUser.photoURL ??
+	// 			"https://peprojects.dev/images/square.jpg",
+	// 		favoriteRestaurants: profile.favoriteRestaurants,
+	// 	})
+	// 		.then(() => {
+	// 			console.log("profile saved");
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log("error", error);
+	// 		});
+	// }
+
+	// watch(
+	// 	current,
+	// 	function () {
+	// 		saveUser();
+	// 	},
+	// 	{ deep: true }
+	// );
 
 	return {
-		profile,
 		current,
 		favoriteRestaurant,
 		favoriteItem,
 		signUp,
 		login,
 		logout,
+		roles,
+		profile,
+		id,
 	};
 });
