af1fee22d3bd59cc85ddeda251de7a216d89b6ed
beejsbj <burooj.bj@gmail.com>
2023-02-20 16:38:58 -0500

    Create a category filter
    still needs to be integrated into favorites

---
+++ src/components/CategoryFilter.vue
@@ -0,0 +1,47 @@
+<script setup>
+	defineProps(["categories"]);
+	const checked = ref([]);
+</script>
+
+
+<template>
+	<category-filter>
+		<ul>
+			<template v-for="category in categories" :key="category.id">
+				<li>
+					<input-field>
+						<input
+							:id="category.id"
+							:value="category"
+							type="checkbox"
+							v-model="checked"
+						/>
+						<label :for="category.id"> {{ category.name }}</label>
+
+					</input-field>
+				</li>
+			</template>
+		</ul>
+		{{checked}}
+	</category-filter>
+</template>
+
+<style scoped>
+category-filter ul {
+	display: flex;
+	gap: 10px;
+}
+input {
+	display: none;
+}
+label {
+	cursor: pointer;
+	background: white;
+	border: 1px solid black;
+	border-radius: 5px;
+	padding: 5px 10px;
+}
+input:checked + label {
+	background: dodgerblue;
+}
+</style>
