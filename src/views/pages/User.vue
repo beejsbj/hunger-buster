bb9d3add77fe41ce2047d774ce79e9e0bf158f83
beejsbj <burooj.bj@gmail.com>
2023-02-20 16:40:00 -0500

    Clean up file

--- src/views/pages/User.vue
+++ src/views/pages/User.vue
@@ -1,20 +1,13 @@
 <script setup>
-	import Profile from "../modules/Profile.vue";
-	import SignUp from "../modules/SignUp.vue";
-
 	import { useUserStore } from "../../stores/user.js";
-	import { RouterLink, RouterView, useRoute } from "vue-router";
 	const user = useUserStore();
 </script>
 
 <template>
-	<SignUp
-		:user="user"
-		v-if="!user.current"
-	/>
+	<section>
+		<Login :user="user" v-if="!user.current" />
 
-	<Profile v-if="user.current" />
-
-	{{ user.current }}
+		<Profile v-if="user.current" />
+	</section>
 </template>
 <style scoped></style>
