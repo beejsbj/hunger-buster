e1664388044acbc2f66985aaed527fe06cbff4a9
beejsbj <burooj.bj@gmail.com>
2023-02-20 16:37:21 -0500

    Display favorites

--- src/views/modules/Favorites.vue
+++ src/views/modules/Favorites.vue
@@ -1,28 +1,24 @@
 <script setup>
-	import { useUserStore } from "../../stores/user.js";
-	import RestaurantCard from "../../components/RestaurantCard.vue";
-	import ItemCard from "../../components/ItemCard.vue";
-
-	const user = useUserStore();
+	defineProps(["profile"]);
 </script>
 <template>
 	<article>
-		<favorites-module v-if="user.profile.favoriteRestaurants">
+		<favorites-module v-if="profile?.favoriteRestaurants">
 			<h2 class="attention-voice">Favorite Restaurants</h2>
 			<ul class="restaurant-list">
 				<li
 					:key="restaurant.id"
-					v-for="restaurant in user.profile.favoriteRestaurants"
+					v-for="restaurant in profile.favoriteRestaurants"
 				>
 					<RestaurantCard :restaurant="restaurant" />
 				</li>
 			</ul>
 		</favorites-module>
-		<favorites-module v-if="user.profile.favoriteItems">
-			<h2 class="attention-voice">Favorite Items</h2>
 
+		<favorites-module v-if="profile?.favoriteItems">
+			<h2 class="attention-voice">Favorite Items</h2>
 			<ul class="items-list">
-				<li :key="item.id" v-for="item in user.profile.favoriteItems">
+				<li :key="item.id" v-for="item in profile.favoriteItems">
 					<ItemCard :item="item" />
 				</li>
 			</ul>
