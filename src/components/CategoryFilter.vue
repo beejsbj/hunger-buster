94baa18537110ad590c3611acdf3e91f5e8a4d6d
beejsbj <burooj.bj@gmail.com>
2023-02-22 21:44:59 -0500

    Emit checked categories from component to parent

--- src/components/CategoryFilter.vue
+++ src/components/CategoryFilter.vue
@@ -1,6 +1,18 @@
 <script setup>
-	defineProps(["categories"]);
-	const checked = ref([]);
+	const props = defineProps(["categories", "checked"]);
+	defineEmits(["update:checked"]);
+
+	function emitChecked(value) {
+		let array = [...props.checked];
+		let index = array.indexOf(value);
+
+		if (index >= 0) {
+			array.splice(index, 1);
+		} else {
+			array.push(value);
+		}
+		return array;
+	}
 </script>
 
 
@@ -12,36 +24,34 @@
 					<input-field>
 						<input
 							:id="category.id"
-							:value="category"
+							:value="category.id"
 							type="checkbox"
-							v-model="checked"
+							@change="$emit('update:checked', emitChecked(category))"
 						/>
 						<label :for="category.id"> {{ category.name }}</label>
-
 					</input-field>
 				</li>
 			</template>
 		</ul>
-		{{checked}}
 	</category-filter>
 </template>
 
 <style scoped>
-category-filter ul {
-	display: flex;
-	gap: 10px;
-}
-input {
-	display: none;
-}
-label {
-	cursor: pointer;
-	background: white;
-	border: 1px solid black;
-	border-radius: 5px;
-	padding: 5px 10px;
-}
-input:checked + label {
-	background: dodgerblue;
-}
+	category-filter ul {
+		display: flex;
+		gap: 10px;
+	}
+	input {
+		display: none;
+	}
+	label {
+		cursor: pointer;
+		background: white;
+		border: 1px solid black;
+		border-radius: 5px;
+		padding: 5px 10px;
+	}
+	input:checked + label {
+		background: dodgerblue;
+	}
 </style>
