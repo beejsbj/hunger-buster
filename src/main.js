f7534bb507a2cebdef2b215f9941336658bc501c
beejsbj <burooj.bj@gmail.com>
2023-02-28 02:34:45 -0500

    tried to use this to use store, not sure if it has an effect

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
+import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
 import { firebaseApp } from "./firebase.js";
 import { VueFire, VueFireAuth } from "vuefire";
-import 'vue3-toastify/dist/index.css';
+import "vue3-toastify/dist/index.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,9 +11,13 @@ import "./styles/site.css";
 
 const app = createApp(App);
 
+const pinia = createPinia();
 
+pinia.use(({ store }) => {
+	store.router = markRaw(router);
+});
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.use(VueFire, {
