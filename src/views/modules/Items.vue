ae125147ff461bb5d5e47f62208172438bd786d3
beejsbj <burooj.bj@gmail.com>
2023-02-28 02:35:57 -0500

    Cleaning and transitions

--- src/views/modules/Items.vue
+++ src/views/modules/Items.vue
@@ -1,20 +1,11 @@
 <script setup>
-	import { collection, query, where } from "firebase/firestore";
-
-	const db = useFirestore();
-	const route = useRoute();
-	const router = useRouter();
 	const shop = useShopStore();
 	const props = defineProps(["restaurant"]);
-
 
 	const search = ref("");
 	const checked = ref([]);
 
-
-	const itemRef = collection(db, "items");
-	const queried = query(itemRef, where("belongsTo", "==", props.restaurant.id));
-	const items = useCollection(queried);
+	const { items } = storeToRefs(shop);
 
 	const categoryFiltered = computed(function () {
 		return items.value.filter(function (item) {
@@ -50,14 +41,19 @@
 			:categories="restaurant?.categories"
 			v-model:checked="checked"
 		/>
-		{{ checked }}
 		<menu-items v-if="items">
-			<ul class="items-list">
-				<li :key="item.id" v-for="item in filtered">
+			<TransitionGroup
+				class="items-list"
+				name="list"
+				tag="ul"
+			>
+				<li
+					:key="item.id"
+					v-for="item in filtered"
+				>
 					<ItemCard :item="item" />
-					<Item :item="item" />
 				</li>
-			</ul>
+			</TransitionGroup>
 		</menu-items>
 	</restaurant-detail>
 </template>
