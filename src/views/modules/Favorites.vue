f40d01aca149c3af2ece29b0f86a126cd83948bc
beejsbj <burooj.bj@gmail.com>
2023-02-28 02:36:39 -0500

    Add transition group

--- src/views/modules/Favorites.vue
+++ src/views/modules/Favorites.vue
@@ -5,23 +5,34 @@
 	<article>
 		<favorites-module v-if="profile?.favoriteRestaurants">
 			<h2 class="attention-voice">Favorite Restaurants</h2>
-			<ul class="restaurant-list">
+			<TransitionGroup
+				name="list"
+				tag="ul"
+				class="restaurant-list"
+			>
 				<li
 					:key="restaurant.id"
 					v-for="restaurant in profile.favoriteRestaurants"
 				>
 					<RestaurantCard :restaurant="restaurant" />
 				</li>
-			</ul>
+			</TransitionGroup>
 		</favorites-module>
 
 		<favorites-module v-if="profile?.favoriteItems">
 			<h2 class="attention-voice">Favorite Items</h2>
-			<ul class="items-list">
-				<li :key="item.id" v-for="item in profile.favoriteItems">
+			<TransitionGroup
+				name="list"
+				tag="ul"
+				class="items-list"
+			>
+				<li
+					:key="item.id"
+					v-for="item in profile.favoriteItems"
+				>
 					<ItemCard :item="item" />
 				</li>
-			</ul>
+			</TransitionGroup>
 		</favorites-module>
 	</article>
 </template>
