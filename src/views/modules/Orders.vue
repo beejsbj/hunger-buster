a94047d25923bb4ed4d45df7ebe76a688c3f7004
beejsbj <burooj.bj@gmail.com>
2023-03-02 21:31:43 -0500

    Init Orders page

---
+++ src/views/modules/Orders.vue
@@ -0,0 +1,33 @@
+<script setup>
+	defineProps(["profile"]);
+</script>
+<template>
+	<article>
+		<h2 class="attention-voice">Order History</h2>
+
+		<TransitionGroup
+			name="list"
+			tag="ul"
+			class="orders-list"
+		>
+			<li
+				:key="order.id"
+				v-for="order in orders"
+			>
+				<!-- cart -->
+				<!-- #TODO  -->
+			</li>
+		</TransitionGroup>
+	</article>
+</template>
+
+<style scoped>
+	article {
+		display: grid;
+		gap: 40px;
+	}
+	orders-list {
+		display: grid;
+		gap: 20px;
+	}
+</style>
