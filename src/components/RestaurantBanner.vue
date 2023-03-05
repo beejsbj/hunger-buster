5f6956d67f78398b441d92e3101efd12269f86fe
beejsbj <burooj.bj@gmail.com>
2023-03-04 19:29:43 -0500

    Move nav out of restaurant banner

--- src/components/RestaurantBanner.vue
+++ src/components/RestaurantBanner.vue
@@ -1,11 +1,5 @@
 <script setup>
-	import { useShopStore } from "../stores/shop";
-	import { useUserStore } from "../stores/user";
-
-	const route = useRoute();
-	const shop = useShopStore();
-	const user = useUserStore();
-	const props = defineProps(["restaurant", "cartCount"]);
+	const props = defineProps(["restaurant"]);
 	const showInfo = ref(false);
 </script>
 
@@ -29,8 +23,6 @@
 				</button>
 			</p>
 
-			<!-- #TODO move to a modal component "more info" -->
-
 			<Teleport to="body">
 				<RestaurantInfo
 					:restaurant="restaurant"
@@ -47,30 +39,6 @@
 				alt="https://peprojects.dev/images/square.jpg"
 			/>
 		</picture>
-		<nav class="restaurant-menu">
-			<RouterLink :to="'/restaurant/' + restaurant.id">Items</RouterLink>
-			<RouterLink
-				:to="'/restaurant/' + restaurant.id + '/cart'"
-				class="cart"
-			>
-				Cart
-				<span :class="{ cartCount: cartCount }">
-					<span>
-						{{ cartCount }}
-					</span>
-				</span>
-			</RouterLink>
-			<RouterLink
-				v-if="user.isAdmin"
-				:to="'/restaurant/' + restaurant.id + '/admin'"
-				>Administator</RouterLink
-			>
-			<RouterLink
-				v-if="user.isBusiness && user.current.uid == restaurant.owner"
-				:to="'/restaurant/' + restaurant.id + '/addItem'"
-				>Business Admin</RouterLink
-			>
-		</nav>
 	</restaurant-banner>
 </template>
 
@@ -80,39 +48,6 @@
 		grid-template-columns: 1fr 0.2fr;
 		picture {
 			grid-row: span 2;
-		}
-		nav {
-			align-content: start;
-		}
-	}
-	a {
-		&.cart {
-			position: relative;
-
-			span {
-				display: none;
-			}
-
-			.cartCount {
-				display: initial;
-				/*	vertical-align: super;*/
-				background: yellow;
-				color: black;
-				position: absolute;
-				top: -10px;
-				right: -5px;
-				padding: 10px;
-				border-radius: 50%;
-
-				span {
-					font-size: 0.9em;
-					display: initial;
-					position: absolute;
-					top: 50%;
-					left: 50%;
-					transform: translate(-50%, -50%);
-				}
-			}
 		}
 	}
 </style>
