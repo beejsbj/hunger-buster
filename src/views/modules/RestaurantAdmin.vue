7a79bda80d7a0ba2e6dcf44746bdf9ec7c1c802d
beejsbj <burooj.bj@gmail.com>
2023-03-04 19:25:47 -0500

    Add Restaurant deleting functionality

--- src/views/modules/RestaurantAdmin.vue
+++ src/views/modules/RestaurantAdmin.vue
@@ -1,10 +1,31 @@
 
+<script setup>
+	const shop = useShopStore();
+	const user = useUserStore();
+</script>
+
 
 <template>
-	<h1 class="loud-voice">
-		RestaurantAdmin
-	</h1>
-	Delete Restaurant
-	Delete Item?
-	editing? renaming?
+	<h1 class="loud-voice">RestaurantAdmin</h1>
+	<!-- edit restaurant button -->
+	<!-- <button
+		class="button"
+		@click="shop.editRestaurant"
+	>
+		Edit Restaurant
+	</button> -->
+
+	<!-- delete restaurant button -->
+	<button
+		class="button danger"
+		@click="shop.deleteRestaurant()"
+	>
+		Delete Restaurant
+	</button>
 </template>
+
+<style scoped>
+	.danger {
+		font-size: 2.5rem;
+	}
+</style>
