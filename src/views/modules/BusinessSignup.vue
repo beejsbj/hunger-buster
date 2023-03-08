9c3ccb026b00e2045c96e8a15539453885eb2523
beejsbj <burooj.bj@gmail.com>
2023-03-08 15:44:48 -0500

    Create Business sign up and login forms

---
+++ src/views/modules/BusinessSignup.vue
@@ -0,0 +1,73 @@
+<script setup>
+	import { reactive } from "vue";
+	import { useFirebaseAuth } from "vuefire";
+
+	const auth = useFirebaseAuth();
+	const user = useUserStore();
+
+	const signupForm = reactive({
+		displayName: "",
+		email: "",
+		password: "",
+	});
+
+	const viewSignupPassword = ref(false);
+</script>
+
+<template>
+	<login-form>
+		<h1 class="loud-voice">Help Hungry People!</h1>
+
+		<form>
+			<h2 class="solid-voice">Business Signup</h2>
+
+			<input-field>
+				<input
+					required
+					id="displayName"
+					v-model="signupForm.displayName"
+					type="text"
+				/>
+				<label for="displayName">Enter Name</label>
+			</input-field>
+			<input-field>
+				<input
+					required
+					id="signup-email"
+					v-model="signupForm.email"
+					type="text"
+				/>
+				<label for="signup-email">Enter Email</label>
+			</input-field>
+			<input-field>
+				<input
+					required
+					id="signup-password"
+					v-model="signupForm.password"
+					:type="viewSignupPassword ? 'text' : 'password'"
+				/>
+				<label for="signup-password">Enter Password</label>
+			</input-field>
+
+			<div class="buttons">
+				<button
+					class="button"
+					@click.prevent="user.businessSignUp(signupForm)"
+				>
+					Sign Up
+				</button>
+				<button
+					class="button"
+					@click="viewSignupPassword = !viewSignupPassword"
+				>
+					<span v-if="!viewSignupPassword">Show Password</span>
+					<span v-if="viewSignupPassword">View Password</span>
+				</button>
+			</div>
+			<RouterLink to="/login">Have an account? Login.</RouterLink>
+		</form>
+	</login-form>
+</template>
+
+<style scoped>
+</style>
