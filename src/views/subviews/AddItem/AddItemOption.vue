b95dc41e77efb9ed39dad4c22e1d47281973007c
beejsbj <burooj.bj@gmail.com>
2023-03-08 15:40:49 -0500

    Rework item options and choices forms to be dynamic

--- src/views/subviews/AddItem/AddItemOption.vue
+++ src/views/subviews/AddItem/AddItemOption.vue
@@ -1,37 +1,24 @@
 
 <script setup>
-	import { useStorage } from "@vueuse/core";
-
 	const props = defineProps(["itemForm"]);
 
-	const option = useStorage("addItemOptionForm", {
-		name: "",
-		choices: [],
-		required: false,
-		multiSelect: false,
-	});
-
 	function addOption() {
-		let record = {
-			name: option.value.name,
-			choices: option.value.choices,
-			required: option.value.required,
-			multiSelect: option.value.multiSelect,
+		let option = {
+			name: "",
+			required: false,
+			multiSelect: false,
+			choices: [],
 		};
 
-		props.itemForm.options.push(record);
-
-		option.value.name = "";
-		option.value.choices = [];
-		option.value.required = false;
-		option.value.multiSelect = false;
+		props.itemForm.options.push(option);
+		reFocus();
 	}
 
 	//to refocus
-	const optionName = ref(null);
+	const optionRef = ref(null);
 	function reFocus() {
 		nextTick(() => {
-			optionName.value.focus();
+			optionRef.value[optionRef.value.length - 1].focus();
 		});
 	}
 </script>
@@ -42,57 +29,66 @@
 		@submit.prevent="addOption()"
 	>
 		<h2 class="solid-voice">Enter restaurant's options.</h2>
-		<input-field>
-			<label for="options"> Enter Option's Name </label>
-			<input
-				type="text"
-				v-model="option.name"
-				id="options"
-				placeholder="Flavor or Size"
-				ref="optionName"
-				required
-			/>
-		</input-field>
-		<input-field class="checkbox">
-			<input
-				type="checkbox"
-				v-model="option.required"
-				id="required"
-			/>
-			<label for="required"> Is this option required? </label>
-		</input-field>
-		<input-field class="checkbox">
-			<input
-				type="checkbox"
-				v-model="option.multiSelect"
-				id="multi-select"
-			/>
-			<label for="multi-select"> Can this option be multi-selected? </label>
-		</input-field>
+		<ul>
+			<li
+				v-for="(option, index) in itemForm.options"
+				:key="index"
+				class="option-field"
+				:style="true ? `--choice-hue: ${index * 20}` : ''"
+			>
+				<input-field>
+					<label for="options"> Enter Option's Name </label>
+					<input
+						type="text"
+						v-model="option.name"
+						id="options"
+						placeholder="Flavor or Size"
+						ref="optionRef"
+						required
+					/>
+				</input-field>
+				<input-field class="checkbox">
+					<input
+						type="checkbox"
+						v-model="option.required"
+						id="required"
+					/>
+					<label for="required"> Is this option required? </label>
+				</input-field>
+				<input-field class="checkbox">
+					<input
+						type="checkbox"
+						v-model="option.multiSelect"
+						id="multi-select"
+					/>
+					<label for="multi-select">
+						Can this option be multi-selected?
+					</label>
+				</input-field>
 
-		<AddOptionChoice :option="option" />
+				<AddOptionChoice :option="option" />
+			</li>
+		</ul>
 
-		<button class="button">
-			Finish adding this option with
-			<span>{{ option.choices.length }}</span>
-			choices
-		</button>
-		<ShowCode :code="option" />
+		<button class="button">Add Option</button>
 	</form>
 </template>
 
 <style scoped>
-	form.add-option {
+	form.add-option ul {
 		/* border: 4px solid red; */
 		display: grid;
 		gap: 10px;
-		background-color: hsla(49, 91%, 86%, 1);
 	}
 
 	input-field.checkbox {
 		display: flex;
 		gap: 10px;
 		align-items: flex-end;
+	}
+
+	.option-field {
+		background-color: hsla(var(--choice-hue), 91%, 90%, 1);
 	}
 </style>
 <style src="@vueform/multiselect/themes/default.css"></style>
--- src/views/subviews/AddItem/AddOptionChoice.vue
+++ src/views/subviews/AddItem/AddOptionChoice.vue
@@ -2,32 +2,27 @@
 <script setup>
 	const props = defineProps(["option"]);
 
-	const choice = reactive({
-		name: "",
-		price: 0,
-		selected: false,
-	});
-
 	function addChoice() {
-		let record = {
-			name: choice.name,
-			price: choice.price,
-			selected: choice.selected,
+		let choice = {
+			name: "",
+			price: 0,
+			selected: false,
 		};
 
-		props.option.choices.push(record);
-
-		choice.name = "";
-		choice.price = 0;
+		props.option.choices.push(choice);
 
 		reFocus();
 	}
 
+	function removeChoice(index) {
+		props.option.choices.splice(index, 1);
+	}
+
 	//to refocus
-	const choiceName = ref(null);
+	const choiceRef = ref(null);
 	function reFocus() {
 		nextTick(() => {
-			choiceName.value.focus();
+			choiceRef.value[choiceRef.value.length - 1].focus();
 		});
 	}
 </script>
@@ -37,47 +32,90 @@
 		class="add-choice"
 		@submit.prevent="addChoice()"
 	>
-		<input-field>
-			<label for="choice-name">Enter choice name</label>
-			<input
-				type="text"
-				v-model="choice.name"
-				id="choice-name"
-				placeholder="Large, Beef , etc."
-				ref="choiceName"
-				required
-			/>
-		</input-field>
-		<input-field>
-			<label for="choice-price">Enter choice price</label>
-			<input
-				type="text"
-				v-model.number="choice.price"
-				id="choice-price"
-				placeholder="Price"
-			/>
-		</input-field>
-		<input-field class="checkbox">
-			<input
-				type="checkbox"
-				v-model="choice.selected"
-				id="selected"
-			/>
-			<label for="selected"> Is this choice selected by default? </label>
-		</input-field>
+		<ul>
+			<li
+				v-for="(choice, index) in option.choices"
+				:key="index"
+				class="choice-field"
+			>
+				<input-field>
+					<label for="choice-name">Choice name</label>
+					<input
+						required
+						type="text"
+						v-model="choice.name"
+						id="choice-name"
+						placeholder="Large, Beef , etc."
+						ref="choiceRef"
+					/>
+				</input-field>
+				<input-field>
+					<label for="choice-price">Choice price</label>
+					<input
+						type="number"
+						step="0.01"
+						min="0"
+						v-model.number="choice.price"
+						id="choice-price"
+						placeholder="Price"
+					/>
+				</input-field>
+				<input-field class="checkbox">
+					<input
+						type="checkbox"
+						v-model="choice.selected"
+						id="selected"
+					/>
+					<label for="selected">
+						Is this choice selected by default?
+					</label>
+				</input-field>
+				<button
+					class="button"
+					@click.prevent="removeChoice(index)"
+				>
+					Remove Choice
+				</button>
+			</li>
+		</ul>
 
 		<button class="button">Add Choice</button>
 	</form>
 </template>
 
 <style scoped>
-	form.add-choice {
+	form.add-choice ul {
 		border: 2px dashed blue;
-		padding: 20px;
+		padding: 10px;
 	}
 	input-field.checkbox {
 		display: flex;
 		gap: 10px;
 		align-items: flex-end;
+	}
+	.choice-field {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 10px;
+		border: 2px dashed red;
+		background-color: hsla(48, 52%, 86%, 1);
+	}
+
+	.choice-field:nth-of-type(odd) {
+		background-color: hsla(48, 92%, 76%, 1);
+	}
+
+	input-field {
+		padding: 0;
+	}
+
+	.choice-field button {
+		justify-self: end;
+		align-self: end;
+	}
+
+	form ul {
+		display: grid;
+		gap: 10px;
 	}
 </style>
