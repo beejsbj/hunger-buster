94890d873c969708b72bc3a13aac46773ef20b94
beejsbj <burooj.bj@gmail.com>
2023-03-08 15:40:57 -0500

    Create User Dashboard

---
+++ src/views/subviews/User/UserDashboard.vue
@@ -0,0 +1,16 @@
+
+<template>
+	<h1 class="attention-voice">UserDashboard</h1>
+	<!-- <button
+		class="button"
+		@click="user.editUser"
+	>
+		Edit User
+	</button> -->
+	<!-- <button
+		class="button danger"
+		@click="user.deleteUser(user.current)"
+	>
+		Delete User
+	</button> -->
+</template>
