f9b4a4eabbd8d481d40b3071daf328b7c028454e
beejsbj <burooj.bj@gmail.com>
2023-03-08 15:46:48 -0500

    Add router guard for links

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { getCurrentUser, useFirestore } from "vuefire";
+import { getDoc, doc } from "firebase/firestore";
 ////////////////////////
 import NotFound from "../views/pages/NotFound.vue";
 import About from "../views/pages/About.vue";
 import Carts from "../views/pages/Carts.vue";
 import Home from "../views/pages/Home.vue";
-import User from "../views/pages/User.vue";
+import User from "../views/pages/UserProfile.vue";
 import Business from "../views/pages/Business.vue";
 import Restaurants from "../views/pages/Restaurants.vue";
 import Restaurant from "../views/pages/Restaurant.vue";
@@ -22,6 +24,16 @@ import Item from "../views/pages/Item.vue";
 
 import Orders from "../views/modules/Orders.vue";
 
+import UserLogin from "../views/modules/UserLogin.vue";
+import UserSignup from "../views/modules/UserSignup.vue";
+
+import BusinessSignup from "../views/modules/BusinessSignup.vue";
+import BusinessLogin from "../views/modules/BusinessLogin.vue";
+
+import UserDashboard from "../views/subviews/user/UserDashboard.vue";
+
+const db = useFirestore();
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -33,61 +45,72 @@ const router = createRouter({
 		{
 			path: "/about",
 			name: "about",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: About,
 		},
 		{
 			path: "/login",
-			name: "login",
-			component: User,
+			name: "userLogin",
+			component: UserLogin,
+		},
+		{
+			path: "/signup",
+			name: "userSignup",
+			component: UserSignup,
+		},
+		{
+			path: "/business-login",
+			name: "businessLogin",
+			component: BusinessLogin,
+		},
+		{
+			path: "/business-signup",
+			name: "businessSignup",
+			component: BusinessSignup,
 		},
 		{
 			path: "/user",
 			name: "user",
 			component: User,
+			meta: { requiresUserAuth: true },
 			children: [
 				{
-					path: "favorites",
-					name: "userFavorites",
-					component: Favorites,
+					path: "/user/dashboard",
+					name: "UserDashboard",
+					component: UserDashboard,
+					alias: "/user",
 				},
 				{
-					path: "orders",
-					name: "userOrders",
-					component: Orders,
-				},
-				{
-					path: "about",
+					path: "/user/about",
 					name: "UserAbout",
 					component: UserAbout,
 				},
 				{
-					path: "restaurants",
+					path: "/user/favorites",
+					name: "userFavorites",
+					component: Favorites,
+				},
+				{
+					path: "/user/orders",
+					name: "userOrders",
+					component: Orders,
+				},
+
+				{
+					path: "/user/restaurants",
 					name: "BusinessRestaurants",
 					component: BusinessRestaurants,
+					alias: "/business",
+					meta: { requiresBusinessAuth: true },
 				},
 				{
-					path: "addRestaurant",
+					path: "/user/addRestaurant",
 					name: "addRestaurant",
 					component: AddRestaurant,
+					alias: "/business/add-restaurant",
+					meta: { requiresBusinessAuth: true },
 				},
 			],
 		},
-		{
-			path: "/business",
-			name: "business",
-			component: Business,
-			children: [
-				{
-					path: "/business/about",
-					name: "BusinessAbout",
-					component: BusinessAbout,
-				},
-			],
-		},
-
 		{
 			path: "/restaurants",
 			name: "Restaurants",
@@ -104,15 +127,15 @@ const router = createRouter({
 			component: Restaurant,
 			children: [
 				{
-					path: "/restaurant/:restaurantSlug/:itemSlug",
-					name: "RestaurantItem",
-					component: Item,
-				},
-				{
 					path: "/restaurant/:restaurantSlug",
 					name: "Items",
 					component: Items,
 					meta: { transition: "slide-to-left" },
+				},
+				{
+					path: "/restaurant/:restaurantSlug/:itemSlug",
+					name: "RestaurantItem",
+					component: Item,
 				},
 				{
 					path: "/restaurant/:restaurantSlug/cart",
@@ -124,13 +147,19 @@ const router = createRouter({
 					path: "/restaurant/:restaurantSlug/admin",
 					name: "RestaurantAdmin",
 					component: RestaurantAdmin,
-					meta: { transition: "slide-to-right" },
+					meta: {
+						transition: "slide-to-right",
+						requiresBusinessOwnerAuth: true,
+					},
 				},
 				{
 					path: "/restaurant/:restaurantSlug/AddItem",
 					name: "AddItem",
 					component: AddItem,
-					meta: { transition: "slide-to-right" },
+					meta: {
+						transition: "slide-to-right",
+						requiresBusinessOwnerAuth: true,
+					},
 				},
 			],
 		},
@@ -140,6 +169,54 @@ const router = createRouter({
 			component: NotFound,
 		},
 	],
+});
+
+router.beforeEach(async (to) => {
+	const currentUser = await getCurrentUser();
+	if (to.meta.requiresUserAuth) {
+		if (!currentUser) {
+			return {
+				path: "/login",
+				query: {
+					redirect: to.fullPath,
+				},
+			};
+		}
+	}
+	if (to.meta.requiresBusinessAuth) {
+		const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+		if (
+			!currentUser ||
+			!userDoc.data().roles.business ||
+			!userDoc.data().roles.admin
+		) {
+			return {
+				path: "/business-login",
+				query: {
+					redirect: to.fullPath,
+				},
+			};
+		}
+	}
+	if (to.meta.requiresBusinessOwnerAuth) {
+		const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+		const restaurantDoc = await getDoc(
+			doc(db, "restaurants", to.params.restaurantSlug)
+		);
+		if (
+			!currentUser ||
+			(!userDoc.data().roles.business &&
+				restaurantDoc.data().owner !== currentUser.uid) ||
+			!userDoc.data().roles.admin
+		) {
+			return {
+				name: "not-found",
+				query: {
+					redirect: to.fullPath,
+				},
+			};
+		}
+	}
 });
 
 export default router;
