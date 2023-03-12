d4248546a67f8724eb4a6396407c28cbdc757f2a
beejsbj <burooj.bj@gmail.com>
2023-03-11 20:17:12 -0500

    Order card

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,29 @@
+
+
+<script setup>
+	const props = defineProps(["order"]);
+</script>
+
+<template>
+	<order-card>
+		<h1 class="solid-voice">Order #{{ order.id }}</h1>
+		<p>
+			{{ order.restaurant.name }}
+		</p>
+		<p>
+			{{ order.restaurant.address.formatted_address }}
+		</p>
+
+		<p>{{ order.deliveryMethod }} : {{ order.deliveryFee }}</p>
+
+		<p>Total: {{ order.total }}</p>
+	</order-card>
+</template>
+
+<style scoped>
+	order-card {
+		display: grid;
+		gap: 10px;
+		background: wheat;
+	}
+</style>
