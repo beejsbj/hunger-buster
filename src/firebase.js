ee9586a1a4de4325c76325794d1095e93fd95642
beejsbj <burooj.bj@gmail.com>
2023-03-13 17:26:15 -0400

    setup firebase file storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_FB_API_KEY,
-   authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-   projectId: import.meta.env.VITE_FB_PROJECT_ID,
-   storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
-   messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-   appId: import.meta.env.VITE_FB_APP_ID,
+	apiKey: import.meta.env.VITE_FB_API_KEY,
+	authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FB_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FB_APP_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+const firebaseStorage = getStorage(firebaseApp);
