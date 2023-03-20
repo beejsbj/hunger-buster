631e392b3bea4071bff91275f5f8cbfc046e735d
beejsbj <burooj.bj@gmail.com>
2023-03-20 19:33:25 -0400

    Refactor into componenet

---
+++ src/views/pages/RestaurantView/subviews/AddressAutoFill.vue
@@ -0,0 +1,22 @@
+<script setup>
+	const user = useUserStore();
+</script>
+
+<template>
+	<div class="gmap-auto-complete">
+		<GMapAutocomplete
+			id="delivery-address"
+			@place_changed="user.setUserLocation"
+			:value="user.address?.formatted_address ?? 'Enter your address'"
+		/>
+		<Skeleton :pill="true" />
+	</div>
+</template>
+
+<style scoped>
+	div.gmap-auto-complete {
+		align-self: center;
+		grid-column: 1 / -1;
+		display: grid;
+	}
+</style>
