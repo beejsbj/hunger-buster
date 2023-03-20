9bb67a5e1f3d4caf0730b55dbe553b3783b0ad96
beejsbj <burooj.bj@gmail.com>
2023-03-20 19:35:41 -0400

    Add heart icon and favoritebuttton component

---
+++ src/components/FavoriteButton.vue
@@ -0,0 +1,58 @@
+<script setup>
+	const props = defineProps(["restaurant", "item", "position"]);
+	const user = useUserStore();
+
+	function onClick() {
+		if (props.restaurant) {
+			user.favoriteRestaurant(props.restaurant);
+		} else if (props.item) {
+			user.favoriteItem(props.item);
+		}
+	}
+
+	const favorited = computed(function () {
+		if (props.restaurant) {
+			return user?.isRestaurantFavorite(props.restaurant);
+		} else if (props.item) {
+			return user?.isItemFavorite(props.item);
+		}
+	});
+</script>
+
+<template>
+	<button
+		class="button favorite"
+		:class="{ favorited: favorited }"
+		@click="onClick"
+	>
+		<picture>
+			<IconHeart />
+		</picture>
+	</button>
+</template>
+
+<style lang="scss">
+	* {
+		&:has(> .favorite) {
+			position: relative;
+
+			& > button.favorite {
+				position: v-bind("position ?? 'absolute'");
+				top: 0px;
+				right: 0px;
+
+				width: 35px;
+				height: 35px;
+				border-radius: 50%;
+
+				padding-top: 6px;
+				box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+				@media (min-width: 450px) {
+					top: 10px;
+					right: 10px;
+				}
+			}
+		}
+	}
+</style>
